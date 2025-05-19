8de9687fbc872dfa67f9717eee961f7d15da0c76
brilanag <brilanartaviagranados@gmail.com>
2025-05-18 19:55:46 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,10 @@ import streamlit as st
 import pandas as pd 
 import matplotlib.pyplot as plt 
 # URLs de los archivos CSV 
-clientes_url = "https://raw.githubusercontent.com/brilanag/Dashboard-Chocolate
-Export/main/clientes.csv" 
-mercados_url = "https://raw.githubusercontent.com/brilanag/Dashboard
-Chocolate-Export/main/mercados.csv" 
-exportaciones_url = "https://raw.githubusercontent.com/brilanag/Dashboard
-Chocolate-Export/main/exportaciones.csv" 
-barreras_url = "https://raw.githubusercontent.com/brilanag/Dashboard-Chocolate
-Export/main/barreras.csv" 
+clientes_url = "https://raw.githubusercontent.com/brilanag/Dashboard-Chocolate-Export/main/clientes.csv" 
+mercados_url = "https://raw.githubusercontent.com/brilanag/Dashboard-Chocolate-Export/main/mercados.csv" 
+exportaciones_url = "https://raw.githubusercontent.com/brilanag/Dashboard-Chocolate-Export/main/exportaciones.csv" 
+barreras_url = "https://raw.githubusercontent.com/brilanag/Dashboard-Chocolate-Export/main/barreras.csv" 
 clientes = pd.read_csv(clientes_url) 
 mercados = pd.read_csv(mercados_url) 
 exportaciones = pd.read_csv(exportaciones_url) 
